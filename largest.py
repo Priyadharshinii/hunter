0085834387dc3481dc8711775fcae6669b64590c
Priyadharshinii <logupriya40@gmail.com>
2017-09-04 21:22:18 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=input("enter first no")
+b=input("enter second no")
+c=input("enter third no")
+if(a>b and a>c):
+print("a is largest")
+elif(b>a and b>c):
+print("b is largest")
+else:
+print("c is largest")
